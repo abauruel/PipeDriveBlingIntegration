9be8a16d44bc399678171053f23101d75ccb032d
abauruel <abauruel@gmail.com>
2020-07-24 18:26:02 -0300

    fix: fixed interface declaration

--- src/modules/deals/services/SaveOpportunitiesInDbService.ts
+++ src/modules/deals/services/SaveOpportunitiesInDbService.ts
@@ -1,9 +1,10 @@
+import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 
 import IOpportunityRepository from '../repositories/IOpportunityRepository';
 
-import IOpportunityIntegrated from '../dtos/IOpportunityIntegrated';
 import IOpportunity from '../dtos/IOpportunity';
+import IOpportunityDayDTO from '../dtos/IOpportunitiesDayDTO';
 
 @injectable()
 class SaveOpportunitiesInDbService {
@@ -13,21 +14,25 @@ class SaveOpportunitiesInDbService {
   ) {}
 
   public async execute(
-    opportunitiesDay: IOpportunityIntegrated[],
-  ): Promise<IOpportunity> {
-    const valueTotal = opportunitiesDay.reduce(
-      (acc, curr) => (acc += Number(curr.totalvenda)),
-      0,
-    );
-    const opportunitiesDayWithTotalValue = {
-      data: opportunitiesDay[0].data,
-      orderNumbers: [...opportunitiesDay],
-      total: valueTotal,
-    };
-    const opportunitiesSaved = await this.opportunityRepository.create(
-      opportunitiesDayWithTotalValue,
-    );
-    return opportunitiesSaved;
+    opportunitiesDay: IOpportunity[],
+  ): Promise<IOpportunityDayDTO> {
+    try {
+      const valueTotal = opportunitiesDay.reduce(
+        (acc, curr) => (acc += Number(curr.totalvenda)),
+        0,
+      );
+      const opportunitiesDayWithTotalValue = {
+        data: opportunitiesDay[0].data,
+        orderNumbers: [...opportunitiesDay],
+        total: valueTotal,
+      };
+      const opportunitiesSaved = await this.opportunityRepository.create(
+        opportunitiesDayWithTotalValue,
+      );
+      return opportunitiesSaved;
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 }
 
