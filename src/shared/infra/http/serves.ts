6bc617ae48e9479aa0e4a9dc593b34fc24af05ba
abauruel <abauruel@gmail.com>
2020-07-24 10:44:14 -0300

    feat: added job scheduled

--- src/shared/infra/http/serves.ts
+++ src/shared/infra/http/serves.ts
@@ -1,8 +1,32 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
+import '../typeorm';
+import '../../container';
+import cron from 'node-cron';
+import routes from './routes';
+
+import AppError from '../../errors/AppError';
 
 const app = express();
 
 app.use(express.json());
+cron.schedule(`${process.env.JOB_MINUTE} ${process.env.JOB_HOUR} * * *`, () =>
+  console.log('Job executado'),
+);
 
-app.listen(3333, () => console.log('Server is running on 333 PORT'));
+app.use(routes);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
+
+app.listen(3333, () => console.log('Server is running on 3333 PORT'));
