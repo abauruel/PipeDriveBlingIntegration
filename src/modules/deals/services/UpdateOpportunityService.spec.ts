c386c8164ea3c5040579554c2d2e77ae045aafbc
abauruel <abauruel@gmail.com>
2020-07-24 23:51:08 -0300

    test: added teste updateOpportunities

---
+++ src/modules/deals/services/UpdateOpportunityService.spec.ts
@@ -0,0 +1,36 @@
+import { uuid } from 'uuidv4';
+import FakeRepository from '../repositories/fakes/FakeOpportunityRepository';
+import UpdateOpportunityService from './UpdateOpportunityService';
+
+describe('UpdateOpportunityService', () => {
+  it('should be able to update opportunity', async () => {
+    const fakeRepository = new FakeRepository();
+    const updateOpportunityService = new UpdateOpportunityService(
+      fakeRepository,
+    );
+
+    const opportunity = await fakeRepository.create({
+      _id: uuid(),
+      data: '22/07/2020',
+      orderNumbers: [],
+      total: 0,
+    });
+
+    const newOpportunity = [
+      {
+        _id: '1',
+        data: '22/07/2020',
+        numero: '1',
+        situacao: 'Em aberto',
+        totalvenda: '10',
+        cliente: 'teste1',
+      },
+    ];
+
+    const opportunityUpdated = await updateOpportunityService.execute(
+      opportunity._id,
+      newOpportunity,
+    );
+    expect(opportunityUpdated.total).toBe(10);
+  });
+});
