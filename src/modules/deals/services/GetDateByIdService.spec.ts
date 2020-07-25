3de30cfc761e0b252c6ccd6c18add94d5d98b5e7
abauruel <abauruel@gmail.com>
2020-07-24 23:56:10 -0300

    test: added new test to getDateIdService

---
+++ src/modules/deals/services/GetDateByIdService.spec.ts
@@ -0,0 +1,18 @@
+import FakeRepository from '../repositories/fakes/FakeOpportunityRepository';
+import GetDateByIdService from './GetDateByIdService';
+
+describe('GetDateByIdService', () => {
+  it('should be able to get date by Id', async () => {
+    const fakeRepository = new FakeRepository();
+    const getDateByIdService = new GetDateByIdService(fakeRepository);
+
+    const opportunity1 = await fakeRepository.create({
+      _id: '1',
+      data: '22/07/2020',
+      orderNumbers: [],
+      total: 0,
+    });
+    const dateFound = await getDateByIdService.execute('1');
+    expect(dateFound).toBe(opportunity1.data);
+  });
+});
